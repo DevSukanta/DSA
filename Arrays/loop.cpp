05ea129472a9ba7a623cede444436e60fdf69569
Sukanta Dev <shlock.dev@gmail.com>
2025-05-11 11:33:29 +0600

    learning DSA arrays

---
+++ Arrays/loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int marks[5]={99, 100, 52, 88, 56};
+   int size = 5;
+
+   //loop
+   for(int i=0; i<size; i++){
+    cout << marks[i] << endl;
+   }
+   return 0;
+}
